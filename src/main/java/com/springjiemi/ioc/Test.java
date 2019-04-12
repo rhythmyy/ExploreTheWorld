339c28664453008acd18aa4e3d319399dca7ba14
yanwu <hello_rhythm@163.com>
2019-04-12 19:03:16 +0800

    4-4 周四

--- src/main/java/com/springjiemi/ioc/Test.java
+++ src/main/java/com/springjiemi/ioc/Test.java
@@ -4,6 +4,10 @@ package com.springjiemi.ioc;
 import com.springjiemi.ioc.fxsystem.FXNewsProvider;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
 /**
  * @author yanwu233
  * @date 2019/3/7 14:50
@@ -15,5 +19,19 @@ public class Test {
         ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("classpath:news-config.xml");
         FXNewsProvider djNewsProvider = (FXNewsProvider)classPathXmlApplicationContext.getBean("djNewsProvider");
         djNewsProvider.getAndPersistNews();
+        throw new AbstractMethodError();
+    }
+
+    @org.junit.Test
+    public void test(){
+        System.out.println(LocalDateTime.of(2099, 12, 31, 23, 59, 59)
+               .toEpochSecond(ZoneOffset.of("+8")));
+        System.out.println(new Date(4102415999L));
+        Date date = new Date(LocalDateTime.of(2099, 12, 31, 23, 59, 59)
+                .toEpochSecond(ZoneOffset.of("+8")) * 1000);
+        System.out.println(date);
+
+        System.out.println(LocalDateTime.of(1970, 1, 1, 0, 0, 0)
+                .toEpochSecond(ZoneOffset.of("+0")));
     }
 }
